84afc20899faa9150b9ef55d41ff405472235ab1
PKopel <pawel.kopel2@gmail.com>
2020-08-19 22:48:32 +0200

    changed types to allow for more flexible operators, list unary operators broken

--- src/Interp/Statements.hs
+++ src/Interp/Statements.hs
@@ -3,117 +3,87 @@
 
 module Interp.Statements where
 
-import           Control.Monad
-import qualified Data.Map                      as Map
-import           Data.Sequence                  ( Seq(..) )
-import qualified Data.Sequence                 as Seq
-import           Import
+import Control.Monad
+import qualified Data.Map as Map
+import Data.Sequence (Seq (..))
+import qualified Data.Sequence as Seq
+import Import
 
 eval :: Expr -> Interp Val
 eval (Val n) = return n
 eval (Var x) = readVar x
-eval (Neg e) = eval e >>= \case
-  AlgVal  v -> return . AlgVal . negate $ v
-  BoolVal v -> return . BoolVal . not $ v
-  _         -> return Null
-eval (AlgBinary  op e1 e2) = evalAlgBin op e1 e2
-eval (BoolBinary op e1 e2) = evalBoolBin op e1 e2
-eval (RelBinary  op e1 e2) = evalRelBin op e1 e2
-eval (ListBinary op e1 e2) = evalListBin op e1 e2
-eval (ListUnary op e     ) = evalListUn op e
-eval (ListLiteral es     ) = ListVal . Seq.fromList <$> mapM eval es
-eval (FunApp n vs        ) = readVar n >>= evalFunApp vs
+eval (Binary op e1 e2) = (appBin op) <$> eval e1 <*> eval e2
+eval (Unary op e) = (appUn op) <$> eval e
+eval (ListLiteral es) = ListVal . Seq.fromList <$> mapM eval es
+eval (FunApp n vs) = readVar n >>= evalFunApp vs
 eval (Print e) =
   mapM eval e >>= mapM printVal >> printString "\n" >> return Null
-eval (Seq []      ) = return Null
-eval (Seq [s     ]) = eval s
+eval (Seq []) = return Null
+eval (Seq [s]) = eval s
 eval (Seq (s : ss)) = eval s >> eval (Seq ss)
-eval (If c e1 e2  ) = eval c >>= \case
-  AlgVal  v -> if v /= 0 then eval e1 else eval e2
-  BoolVal v -> if v then eval e1 else eval e2
-  ListVal v -> if not (Seq.null v) then eval e1 else eval e2
-  _         -> return Null
-eval (While e s         ) = ListVal <$> evalWhile e s Empty
-eval (Assign x Nothing e) = do
-  v <- eval e
-  writeVar x v
-eval (Assign x (Just i) e) = do
+eval (If c e1 e2) =
+  eval c >>= \case
+    AlgVal v -> if v /= 0 then eval e1 else eval e2
+    BoolVal v -> if v then eval e1 else eval e2
+    ListVal v -> if not (Seq.null v) then eval e1 else eval e2
+    _ -> return Null
+eval (While e s) = ListVal <$> evalWhile e s Empty
+eval (Assign x i e) = do
   v <- eval e
   readVar x >>= \case
-    ListVal l -> evalAlg i >>= \case
-      Nothing  -> writeVar x v
-      Just ind -> writeVar x . ListVal $ Seq.update (round ind) v l
+    ListVal l ->
+      eval i >>= \case
+        AlgVal ind -> writeVar x . ListVal $ Seq.update (round ind) v l
+        _ -> writeVar x v
     _ -> writeVar x v
-
-evalAlg :: Expr -> Interp (Maybe Double)
-evalAlg e = eval e >>= return . algValToMaybe
-
-evalBool :: Expr -> Interp (Maybe Bool)
-evalBool e = eval e >>= return . boolValToMaybe
 
 evalList :: Expr -> Interp (Maybe (Seq Val))
 evalList e = eval e >>= return . listValToMaybe
 
-evalAlgBin :: AlgBinOp -> Expr -> Expr -> Interp Val
-evalAlgBin op e1 e2 = do
-  v1 <- evalAlg e1
-  v2 <- evalAlg e2
-  return . maybeToAlgVal $ op <$> v1 <*> v2
-
-evalBoolBin :: BoolBinOp -> Expr -> Expr -> Interp Val
-evalBoolBin op e1 e2 = do
-  v1 <- evalBool e1
-  v2 <- evalBool e2
-  return . maybeToBoolVal $ op <$> v1 <*> v2
-
-evalRelBin :: RelBinOp -> Expr -> Expr -> Interp Val
-evalRelBin op e1 e2 = do
-  v1 <- evalAlg e1
-  v2 <- evalAlg e2
-  return . maybeToBoolVal $ op <$> v1 <*> v2
-
-evalListBin :: ListBinOp -> Expr -> Expr -> Interp Val
-evalListBin op e1 e2 = do
-  l <- eval e1
-  v <- eval e2
-  return . maybeToListVal $ op <$> toMaybeList l <*> toMaybeList v
-  where toMaybeList = Just . valToList
-
-evalListUn :: ListUnOp -> Expr -> Interp Val
+evalListUn :: (Seq Val -> (Val, Seq Val)) -> Expr -> Interp Val
 evalListUn op e = do
   let (iv, x) = isVar e
   evalList e >>= return . fmap op >>= \case
     Just (h, t) -> when iv (writeVar x (ListVal t) >> return ()) >> return h
-    _           -> return Null
+    _ -> return Null
 
 evalFunApp :: [Expr] -> Val -> Interp Val
+-- execute function of name n with arguments from vs
 evalFunApp vs (FunVal as body) = do
-  -- execute function of name n with arguments from vs
   newLocals <-
     Map.fromList <$> zipWithM (\a v -> eval v >>= return . (,) a) as vs
   oldLocals <- getStore >>= return . view (scope localL)
-  oldScope  <- getScope
+  oldScope <- getScope
   withStore $ (over . scope) localL (const newLocals) -- introducing local variables
   setScope localL -- from now variables are declared in local scope
   retVal <- eval body
   setScope oldScope -- end of local scope
   withStore $ (over . scope) localL (const oldLocals) -- removing local variables
   return retVal
+-- get elements from list of name n
 evalFunApp vs (ListVal l) = do
-  -- get elements from list of name n
-  evs <- foldM (\acc v -> evalAlg v >>= return . (: acc)) [] vs
+  evs <-
+    foldM
+      ( \acc v ->
+          eval v >>= \case
+            AlgVal av -> return (av : acc)
+            _ -> return acc
+      )
+      []
+      vs
   case getElems l evs of
-    v  :<|    Empty -> return v -- one argument in vs, result is a single value
-    v@(_ :<| _)     -> return $ ListVal v -- more than one argument in vs, result is a list
-    _               -> return Null
+    v :<| Empty -> return v -- one argument in vs, result is a single value
+    v@(_ :<| _) -> return $ ListVal v -- more than one argument in vs, result is a list
+    _ -> return Null
 evalFunApp _ _ = return Null
 
 evalWhile :: Expr -> Expr -> Seq Val -> Interp (Seq Val)
-evalWhile e s acc = eval e >>= \case
-  AlgVal  v -> while $ v /= 0
-  BoolVal v -> while v
-  ListVal v -> while $ not (Seq.null v)
-  _         -> return acc
- where
-  while cond =
-    if cond then eval s >>= evalWhile e s . (acc Seq.|>) else return acc
+evalWhile e s acc =
+  eval e >>= \case
+    AlgVal v -> while $ v /= 0
+    BoolVal v -> while v
+    ListVal v -> while $ not (Seq.null v)
+    _ -> return acc
+  where
+    while cond =
+      if cond then eval s >>= evalWhile e s . (acc Seq.|>) else return acc
--- src/Lang/Parser.hs
+++ src/Lang/Parser.hs
@@ -4,45 +4,47 @@
 
 module Lang.Parser where
 
-import           Control.Monad
-import           Import                  hiding ( many
-                                                , optional
-                                                , try
-                                                , (<|>)
-                                                )
-import           Lang.Lexer
-import           Text.Parsec
-import           Text.Parsec.String
-import           Text.ParserCombinators.Parsec.Expr
+import Control.Monad
+import Import hiding
+  ( many,
+    optional,
+    try,
+    (<|>),
+  )
+import Lang.Lexer
+import Text.Parsec
+import Text.Parsec.String
+import Text.ParserCombinators.Parsec.Expr
 
 parseProg :: String -> Either String Prog
 parseProg p = case parse progParser "PiG" p of
-  Left  err  -> Left $ show err
+  Left err -> Left $ show err
   Right prog -> Right prog
 
 parseFile :: FilePath -> IO (Either String [Prog])
-parseFile f = parseFromFile (sepEndBy1 progParser semi <* eof) f >>= \case
-  Left  err  -> return . Left $ show err
-  Right prog -> return $ Right prog
+parseFile f =
+  parseFromFile (sepEndBy1 progParser semi <* eof) f >>= \case
+    Left err -> return . Left $ show err
+    Right prog -> return $ Right prog
 
 progParser :: Parser Prog
 progParser = whiteSpace >> (Stmt <$> seqExprParser <|> Drct <$> drctParser)
 
 endParser :: ParsecT String u Identity ()
 endParser =
-  lookAhead
-    $  whiteSpace
-    <* (   skipMany1 semi
-       <|> skipMany1 endOfLine
-       <|> skipMany1 (char ')')
-       <|> skipMany1 (char '}')
-       <|> skipMany1 (char ']')
-       <|> skipMany1 (char ',')
-       <|> skipMany1 (reserved "then")
-       <|> skipMany1 (reserved "else")
-       <|> skipMany1 (reserved "do")
-       <|> eof
-       )
+  lookAhead $
+    whiteSpace
+      <* ( skipMany1 semi
+             <|> skipMany1 endOfLine
+             <|> skipMany1 (char ')')
+             <|> skipMany1 (char '}')
+             <|> skipMany1 (char ']')
+             <|> skipMany1 (char ',')
+             <|> skipMany1 (reserved "then")
+             <|> skipMany1 (reserved "else")
+             <|> skipMany1 (reserved "do")
+             <|> eof
+         )
 
 last :: Parser a -> Parser a
 last b = b <* endParser
@@ -75,21 +77,21 @@ singleExprParser = braces seqExprParser <|> try exprParser <|> printExprParser
 
 ifExprParser :: Parser Expr
 ifExprParser =
-  (do
-      cond  <- reserved "if" >> exprParser
+  ( do
+      cond <- reserved "if" >> exprParser
       stmt1 <- reserved "then" >> singleExprParser
       stmt2 <- option (Val Null) (reserved "else" >> singleExprParser)
       return $ If cond stmt1 stmt2
-    )
+  )
     <?> "if"
 
 whileExprParser :: Parser Expr
 whileExprParser =
-  (do
+  ( do
       cond <- reserved "while" >> exprParser
       stmt <- reserved "do" >> singleExprParser
       return $ While cond stmt
-    )
+  )
     <?> "while"
 
 printExprParser :: Parser Expr
@@ -105,19 +107,19 @@ algValParser =
 boolValParser :: Parser Val
 boolValParser =
   BoolVal
-    <$> (   try (reserved "true" >> return True)
-        <|> try (reserved "false" >> return False)
+    <$> ( try (reserved "true" >> return True)
+            <|> try (reserved "false" >> return False)
         )
     <|> (reserved "null" >> return Null)
 
 funValParser :: Parser Val
 funValParser =
-  (do
+  ( do
       args <- parens (commaSep identifier)
       reservedOp "=>"
       body <- singleExprParser
       return $ FunVal args body
-    )
+  )
     <?> "function definition"
 
 listValParser :: Parser Expr
@@ -140,21 +142,21 @@ funExprParser = try funAppParser <|> Val <$> funValParser
 
 funAppParser :: Parser Expr
 funAppParser =
-  (do
+  ( do
       name <- identifier
       args <- parens (commaSep exprParser)
       return $ FunApp name args
-    )
+  )
     <?> "function application"
 
 assignExprParser :: Parser Expr
 assignExprParser =
-  (do
-      var   <- identifier
-      index <- option Nothing (Just <$> parens exprParser)
-      expr  <- reservedOp "=" >> exprParser
+  ( do
+      var <- identifier
+      index <- option (Val Null) (parens exprParser)
+      expr <- reservedOp "=" >> exprParser
       return $ Assign var index expr
-    )
+  )
     <?> "assignment"
 
 algTerm :: ParsecT String () Identity Expr
@@ -181,9 +183,9 @@ relExprParser = do
   a1 <- algExprParser
   op <- relation
   a2 <- algExprParser
-  return $ RelBinary op a1 a2
+  return $ Binary op a1 a2
 
-relation :: ParsecT String u Identity RelBinOp
+relation :: ParsecT String u Identity (Double -> Double -> Bool)
 relation =
   (reservedOp ">" >> return (>))
     <|> (reservedOp "<" >> return (<))
