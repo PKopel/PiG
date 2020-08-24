ffad59d1cbdab008804c6296c06a531960775e07
PKopel <pawel.kopel2@gmail.com>
2020-08-24 14:02:45 +0200

    add TypesSpec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,12 @@
 {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+
+{-
+import Test.Hspec
+import TypesSpec
+import UtilSpec
+
+main :: IO ()
+main = hspec $ do
+  UtilSpec.spec
+  TypesSpec.spec
+  -}
--- test/UtilSpec.hs
+++ test/UtilSpec.hs
@@ -2,10 +2,28 @@
 
 module UtilSpec (spec) where
 
-import Import
+import Import hiding (assert)
 import Test.Hspec
-import Test.Hspec.QuickCheck
-import Utils.Util
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+import TypesSpec ()
+
+prop_putGetStore :: Store -> Gen (Interp Property)
+prop_putGetStore s = monadic' $ do
+  s' <- run (putStore s >> getStore)
+  assert (s == s')
+
+prop_setGetScope :: Scope -> Store -> Gen (Interp Property)
+prop_setGetScope sc st = monadic' $ do
+  sc' <- run (setScope sc >> getScope)
+  let v = view (scope sc) st
+      v' = view (scope sc') st
+  assert (v == v')
+
+prop_withStore :: Store -> Gen (Interp Property)
+prop_withStore s = monadic' $ do
+  s' <- run (putStore s >> withStore id >> getStore)
+  assert (s == s')
 
 spec :: Spec
 spec = undefined {- do
