f0cb0e3dea53cfdf7ad076fc090945064ff38af8
Paweł Kopel <pawel.kopel2@gmail.com>
2023-11-29 22:24:42 +0100

    Refactor dependencies and build workflow (#47)
    * Remove unused import, add -dynamic flag
    
    * remove -dynamic flag
    
    * add strip stage in release
    
    * update ChangeLog.md
    
    * add split-sections build optino
    
    * fix build on windows
    
    * Switch release workflow to use stack

--- src/Lang/BIF/Str.hs
+++ src/Lang/BIF/Str.hs
@@ -3,8 +3,8 @@
 module Lang.BIF.Str where
 
 
-import qualified Data.Sequence                 as Seq
 import           RIO
+import qualified RIO.Seq                       as Seq
 import           Utils.Types                    ( Interp
                                                 , Val(..)
                                                 )
@@ -14,5 +14,5 @@ strToNum, strToList :: [Val] -> Interp a Val
 strToNum ((StrVal str) : _) = return $ maybe Null AlgVal (readMaybe str)
 strToNum _                  = return Null
 strToList ((StrVal str) : _) =
-    return . ListVal . Seq.fromList . map CharVal $ str
+  return . ListVal . Seq.fromList . map CharVal $ str
 strToList _ = return Null
